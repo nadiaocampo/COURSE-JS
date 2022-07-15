8b7d9a9b52ba40c308582f674a0542bd6a6d7c97
Nadia Ocampo <nsocampo@hotmail.com>
2022-07-15 13:52:09 -0300

    Add files via upload

---
+++ leccion-notacion.js
@@ -0,0 +1,13 @@
+const yo= {
+    nombre: "Nadia",
+    edad= 37,
+    desarrollador= true,
+    nacimiento= new Date (0, 10, 1985),
+    
+    librofavorito= {
+    titulo: "La vida es Bella",
+    Autor: "Antoine de Saint-Exupéry",
+    fecha: Date(04, 1943),
+    url: "https://www.culturagenial.com/es/libro-el-principito/"
+}
+}
